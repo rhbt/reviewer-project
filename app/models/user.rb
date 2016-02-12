ea426b26cea197d51a5603ab3078b61799ade2a4
Ryan Ho <rhbt.dev@gmail.com>
2016-02-12 21:57:32 +0000

    Implement basic user sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ActiveRecord::Base
+    before_save :downcase_email
+    
+    validates :username, presence: true, length: { minimum: 3, maximum: 30 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }, 
+                    uniqueness: { case_sensitive: false }
+    has_secure_password 
+    validates :password, presence: true, length: { minimum: 6 }
+        
+    private 
+    
+      def downcase_email
+          self.email = email.downcase
+      end
+      
 end
