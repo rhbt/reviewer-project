95e65c1e420af3bafb9daca833cd99aa75e2c95e
Ryan Ho <ryan.ho.projects@gmail.com>
2016-02-12 21:57:32 +0000

    Implement basic user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get 'about' => 'static_pages#about'
-  
-  get 'signup' => 'users#new'
+  get 'signup'  => 'users#new'
+  resources :users, only: [:new, :create]
 
 end
