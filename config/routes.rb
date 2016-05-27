e4b1340375f408111c92081d5894606e13ef11f9
Ryan Ho <rhbt.dev@gmail.com>
2016-05-27 21:36:42 +0000

    Add footer and help and contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   
   root 'static_pages#home'
   get 'about' => 'static_pages#about'
+  get 'help'    => 'static_pages#help'
+  get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
