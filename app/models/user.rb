3d89d5d7e56934639cc08e46228110c99714e523
Ryan Ho <rhbt.dev@gmail.com>
2016-05-27 05:47:26 +0000

    add user functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,50 @@
 class User < ActiveRecord::Base
-    has_many :reviews
-    before_save :downcase_email
+  attr_accessor :remember_token
+  
+  has_many :reviews
+  before_save :downcase_email
     
-    validates :username, presence: true, length: { minimum: 3, maximum: 30 }
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX }, 
-                    uniqueness: { case_sensitive: false }
-    has_secure_password 
-    validates :password, presence: true, length: { minimum: 6 }
-        
-    private 
+  validates :username, presence: true, length: { minimum: 3, maximum: 30 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                  format: { with: VALID_EMAIL_REGEX }, 
+                  uniqueness: { case_sensitive: false }
+  has_secure_password 
+  validates :password, presence: true, length: { minimum: 6 }
+
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Returns a random token.
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
     
-      def downcase_email
-          self.email = email.downcase
-      end
+  # Remembers a user in the database for use in persistent sessions.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  private 
+    
+    def downcase_email
+      self.email = email.downcase
+    end
       
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get 'about' => 'static_pages#about'
   get 'signup'  => 'users#new'
-  get 'signin' => 'sessions#new'
-  post 'signin' => 'sessions#create'
-  resources :users, only: [:new, :create, :index]
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+  resources :users
   resources :reviews, only: [:index, :new, :create, :destroy]
 
 end
