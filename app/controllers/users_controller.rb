75c56138d4dc64e9967843830cf8f06617e386e5
Ryan Ho <ryan.ho.projects@gmail.com>
2016-05-27 05:47:26 +0000

    add user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  
   def index
     redirect_to new_user_path
+  end
+  
+  def show
+    @user = User.find(params[:id])
   end
   
   def new
@@ -16,10 +23,34 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
   private
     
     def user_params
       params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+    
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
     end
     
 end
