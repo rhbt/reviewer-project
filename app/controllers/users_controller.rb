79c53ce27de8cd503259a2db375985445d8c318c
Ryan Ho <rhbt.dev@gmail.com>
2016-06-07 23:50:25 +0000

    Add account activation and password reset

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     
     if @user.save
-      log_in(@user)
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
     else
       render "new"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160603014945) do
+ActiveRecord::Schema.define(version: 20160607233652) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,12 +65,17 @@ ActiveRecord::Schema.define(version: 20160603014945) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
     t.string   "password_digest"
     t.string   "remember_digest"
     t.datetime "last_review"
     t.datetime "last_comment"
+    t.string   "activation_digest"
+    t.boolean  "activated",         default: false
+    t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
