806a92d88cd4bbfaa314cf47b1929d32c924d1dc
Ryan Ho <ryan.ho.projects@gmail.com>
2016-06-07 23:50:25 +0000

    Add account activation and password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   root 'static_pages#home'
   get "about" => "static_pages#about"
   get "help"    => "static_pages#help"
@@ -14,5 +18,8 @@ Rails.application.routes.draw do
   resources :reviews, only: [:index, :new, :create, :destroy, :show]
   resources :stickied_reviews, only: [:create, :destroy]
   resources :comments, only: [:create, :destroy]
+  
+  resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 
 end
